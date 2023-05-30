2d919005fab514a29cd3ae1cd737c721e4503cdc
Beer Koala <beer_koala@icloud.com>
2023-05-31 00:10:45 +0200

    Show in table all languages

--- FLAnGuage/View/SettingsViewController.swift
+++ FLAnGuage/View/SettingsViewController.swift
@@ -9,4 +9,35 @@ import Cocoa
 
 class SettingsViewController: NSViewController {
 
+    @IBOutlet weak var inputSourceLanguages: NSScrollView?
+
+}
+
+extension SettingsViewController: NSTableViewDelegate {
+
+}
+
+extension SettingsViewController: NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        LanguageManager.shared.allLanguages.count
+    }
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+
+        let inputSource = LanguageManager.shared.allLanguages[row]
+
+        guard let cell = tableView.makeView(
+            withIdentifier: tableColumn!.identifier,
+            owner: self
+        ) as? NSTableCellView else { return nil }
+
+        if (tableColumn?.identifier)!.rawValue == "InputSource" {
+            cell.textField?.stringValue = inputSource.name
+        } else if (tableColumn?.identifier)!.rawValue == "Title" {
+            cell.textField?.stringValue = inputSource.statusBarName
+        }
+
+        return cell
+    }
 }
